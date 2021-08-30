e26fff7b99f3905896d0b5a9f0f4418acdc9d4c0
geobontognali <geo.bontognali@gmail.com>
2021-08-30 22:11:57 +0200

    Here I have the dependency instanciator (called serviceprovider in .NET Core) and the dependency container (DIC). It is possible to instanciate objects and dependencies are injected automatically recursively

--- GeosDIC/Program.cs
+++ GeosDIC/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 /*
  * My own implementation of the .NET Core dependecy injector container. Trying to get a deeper understanding of it functioning.
@@ -7,9 +8,10 @@ using System.Collections.Generic;
  * 
  * stage0 = Base Program without dependency inversion.
  * stage1 = In this version, we inverted the dipendencies and instanciated programmatically instead of using the *new* keyword. The process ist still manual though
+ * stage2 = Here I have the first dependency instanciator (called serviceprovider in .NET Core). It is possible to instanciate objects and dependencies are injected automatically (no recursion yet)
  */
 
-// Here I have the first dependency instanciator (called serviceprovider in .NET Core). It is possible to instanciate objects and dependencies are injected automatically (no recursion yet)
+// Here I have the dependency instanciator (called serviceprovider in .NET Core) and the dependency container (DIC). It is possible to instanciate objects and dependencies are injected automatically recursively
 
 namespace GeosDIC
 {
@@ -17,8 +19,15 @@ namespace GeosDIC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Instanciating the Dependency Instanciator/Injector...");
-            var dependencyInstanciator = new DependencyInstanciator(); // Create Instanciator (ServiceProvider, Object Spawner, whatever you wanna call it)   
+            Console.WriteLine("Instanciating the Dependency Instanciator and Container...");
+            var dependencyContainer = new DependencyContainer();
+            var dependencyInstanciator = new DependencyInstanciator(dependencyContainer); // Create Instanciator (ServiceProvider, Object Spawner, whatever you wanna call it)   
+
+            Console.WriteLine("Registering dependencies...");
+            dependencyContainer.AddDependency<Car>();
+            dependencyContainer.AddDependency<Stereo>();
+            dependencyContainer.AddDependency<Heater>();
+            dependencyContainer.AddDependency<CD>();
 
             Console.WriteLine("Instanciating the Car object and auto-injecting dependencies...");
             dependencyInstanciator.Instanciate<Car>(); // This spawns the car object and injects its dependencies automatically
@@ -27,34 +36,78 @@ namespace GeosDIC
         }
     }
 
+    // The dependency container lists the registered dependencies. The list is used to differentiate between dependencies and "normal" constructor Parameter at the moment of creation.
+    public class DependencyContainer
+    {
+        public List<Type> dependencies;
+
+        public DependencyContainer()
+        {
+            this.dependencies = new List<Type>();
+        }
+
+        // Adds dependencies to the container
+        public void AddDependency<T>()
+        {
+            dependencies.Add(typeof(T));
+        }
+        // Gets dependencies from the container. In actuality it checks if the given type is registered as dependency, if not return a null object.
+        public Type GetDependency(Type dependency)
+        {
+            return dependencies.Find(x => x.Name == dependency.Name);
+        }
+    }
+
     // Automatically instanciate objects and injects dependency. Only the parent object needs to be given.
     public class DependencyInstanciator
     {
-        public T Instanciate<T>()
+        private DependencyContainer dependencyContainer;
+
+        public DependencyInstanciator(DependencyContainer dependencyContainer)
         {
-            // Check if and how many parameters the constructor needs. For simplicity only the first constructor overload is analyzed and only dependencies as parameter are expected
-            var constructorParameters = typeof(T).GetConstructors()[0].GetParameters(); // Return an array of parameters (the dependencies we need to inject)
+            this.dependencyContainer = dependencyContainer;
+        }
+
+        // Returns the instanciated object type given
+        public object Instanciate(Type dependencyType)
+        {
+            // Check if and how many parameters the constructor needs. For simplicity only the first constructor overload is searched
+            var constructorParameters = dependencyType.GetConstructors()[0].GetParameters(); // Return an array of parameters (the dependencies we need to inject)
             
-            // If there are parameters (dep to inject
+            // If there are parameters, go trough them
             if(constructorParameters.Length > 0)
             {
-                List<object> instanciatedConstructorParameters = new List<object>();
-                foreach(var parameter in constructorParameters) // goes through all parameters, instanciate objects off of those parameters (dependencies) and adds the objects to a list
+                List<object> instanciatedConstructorParameters = new List<object>(); // here are stored the instanciated dependencies 
+                // goes through all parameters, check if are known dependencies, if so instanciate objects off of those parameters and adds the objects to the list
+                foreach (var parameter in constructorParameters) 
                 {
-                    instanciatedConstructorParameters.Add(Activator.CreateInstance(parameter.ParameterType));
+                    // check if its a known dependency
+                    Type dependency = dependencyContainer.GetDependency(parameter.ParameterType);
+                    if(dependency != null)
+                    {
+                        instanciatedConstructorParameters.Add(Instanciate(dependency));
+                    }
                 }
-                return (T)Activator.CreateInstance(typeof(T), instanciatedConstructorParameters[0], instanciatedConstructorParameters[1]); // Instanciate the wished class with the parameters (the dependencies)
+                var args = instanciatedConstructorParameters.Cast<object>().ToArray(); // Convert the object list to an array, used next in this format
+                return Activator.CreateInstance(dependencyType, args); // Instanciate the wished class with the parameters (the dependencies)
             }
             // Else simply instanciate
             else
             {
-                return (T)Activator.CreateInstance(typeof(T));
+                return Activator.CreateInstance(dependencyType);
             }
+        }
+
+        // overloaded method with generic call (for the first/top call, not really necessary but more efficient)
+        public T Instanciate<T>()
+        {
+            return (T)Instanciate(typeof(T));
         }
     }
 
 
-    // Example Classes
+
+    // Example Classes *******************************************************************************************************************
     // Parent Object
     public class Car
     {
@@ -66,10 +119,10 @@ namespace GeosDIC
         public Car(Stereo stereo, Heater heater)
         {
             carID = Guid.NewGuid();
-            Console.WriteLine($"Car instanciated. SerialNr: #{ carID }");
             this.stereo = stereo;
             this.heater = heater;
-            Console.WriteLine($"This car has the Stereo #{this.stereo.stereoID } and the Heater #{ this.heater.heaterID}.");
+            Console.WriteLine($"Car created. Serial Nr: #{ carID }");
+            Console.WriteLine($"and the Stereo #{ this.stereo.stereoID } and the Heater #{ this.heater.heaterID } where added.");
         }
     }
 
@@ -77,11 +130,13 @@ namespace GeosDIC
     public class Stereo
     {
         public Guid stereoID;
+        public CD media;
 
-        public Stereo()
+        public Stereo(CD disc)
         {
             this.stereoID = Guid.NewGuid();
-            Console.WriteLine($"Stereo instanciated. SerialNr: #{ this.stereoID }");
+            this.media = disc;
+            Console.WriteLine($"Stereo created. SerialNr: #{ this.stereoID }");
         }
 
     }
@@ -94,7 +149,24 @@ namespace GeosDIC
         public Heater()
         {
             this.heaterID = Guid.NewGuid();
-            Console.WriteLine($"Car heater instantiated. SerialNr: #{ this.heaterID }");
+            Console.WriteLine($"Heater created. SerialNr: #{ this.heaterID }");
+        }
+    }
+
+    // Third Dependency (Recursive)
+    public class CD
+    {
+        private Guid disc_ID;
+
+        public CD()
+        {
+            this.disc_ID = Guid.NewGuid();
+            Console.WriteLine($"Disc added. SerialNr: #{ this.disc_ID }");
+        }
+
+        public Guid GetID()
+        {
+            return disc_ID;
         }
     }
 }
