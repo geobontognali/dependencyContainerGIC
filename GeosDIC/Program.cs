7b902f2441bdea9adb6f6af64d5c0debdd6c288d
geobontognali <geo.bontognali@gmail.com>
2021-08-30 13:23:08 +0200

    Add project files.

---
+++ GeosDIC/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+ * My own implementation of the C# dependecy injector container. Trying to get a deeper understanding of it functioning.
+ */
+namespace GeosDIC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Started");
+
+
+        }
+    }
+
+
+    class Car
+    {
+        public Car()
+        {
+            Wheel
+        }
+    }
+
+    class Wheel
+    {
+        public Wheel()
+        {
+
+        }
+    }
+}
