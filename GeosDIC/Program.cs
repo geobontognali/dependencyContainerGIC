63eb18bb786437dbd00b061c0cb1ea8625da5acc
geobontognali <geo.bontognali@gmail.com>
2021-08-30 16:32:19 +0200

    dependency inversion & programmatical instanciation

--- GeosDIC/Program.cs
+++ GeosDIC/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /*
  * My own implementation of the C# dependecy injector container. Trying to get a deeper understanding of it functioning.
+ * See the different .stageX files to track the changes.
+ * 
+ * stage0 = Base Program without dependency inversion.
  */
 
+// In this version, we inverted the dipendencies and instanciated programmatically instead of using the *new* keyword. The process ist still manual though
+// Furthermore you would work with interfaces to further decouple the dependencies. For simplicy this is neglected here.
 
 namespace GeosDIC
 {
@@ -13,27 +19,32 @@ namespace GeosDIC
         {
             Console.WriteLine("Program Started");
 
-            Car car = new Car();
+            // This is basically the same as using the keyword "new"
+            Stereo stereo = (Stereo) Activator.CreateInstance(typeof(Stereo), "Sony", 1500);
+            // Same here
+            Car car = (Car) Activator.CreateInstance(typeof(Car), stereo);
         }
     }
 
-    // In this basic version, Car has a dependency on Stereo. 
     class Car
     {
-        int carId;
-        public Car()
+        public Stereo stereo;
+
+        public Car(Stereo stereo)
         {
-            carId = new Random().Next();
-            Console.WriteLine($"Car #{ carId } instanciated.");
-            var stereo = new Stereo("Sony", 18);
+            carID = Guid.NewGuid();
+            Console.WriteLine($"Car #{ carID } instanciated.");
+            this.stereo = stereo;
         }
+
+        public Guid carID { get; private set; }
     }
 
     class Stereo
     {
         public string model { get; }
         public int wattage { get; }
-
+     
         public Stereo(string model, int wattage)
         {
             this.model = model;
